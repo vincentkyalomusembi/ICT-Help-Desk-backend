9c02b25a7f48a8641d0eef01b249ab6b7121dd4a
SoonAnthony <anthonymedia062@gmail.com>
2026-05-21 21:06:45 +0300

    Create main.py for the project

---
+++ main.py
@@ -0,0 +1,14 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.db.database import check_db_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await check_db_connection()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def home():
+    return {"message": "Hello, World!"}
