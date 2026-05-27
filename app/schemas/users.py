a37b9028b5dcd4467060e4fd58f6814637821a0a
SoonAnthony <anthonymedia062@gmail.com>
2026-05-27 11:07:23 +0300

    Update users schema

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,42 +1,41 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
+from app.models import UserRole
 from uuid import UUID
-from app.models.users import UserRole
-
-
 class UserCreate(BaseModel):
     personal_no: str
-    first_name: str
-    last_name: str
     email: EmailStr
-    password: str
-    phone_number: str
-    directorate_id: int
-    department_id: int
-    office_number: str
+    first_name: str
+    last_name: str 
+    phone: str
+    directorate_id: int 
+    department_id: int 
+    office_number:str 
     office_location: str
+    password: str
+
 
 class UserResponse(BaseModel):
     auth_user_id: UUID
     personal_no: str
-    first_name: str
-    last_name: str
     email: EmailStr
-    directorate_id: int
-    department_id: int
-    office_number: str
+    first_name: str
+    last_name: str 
+    phone: str
+    directorate_id: int 
+    department_id: int 
+    office_number:str 
     office_location: str
     role: UserRole
     is_activated: bool
     is_active: bool
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UserUpdate(BaseModel):
     first_name: str | None = None
     last_name: str | None = None
-    office_number: str | None = None
-    office_location: str | None = None
     directorate_id: int | None = None
     department_id: int | None = None
+    office_number:str | None = None
+    office_location: str | None = None
