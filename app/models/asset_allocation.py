12e4194e4f44e62445c9033897a49d76aa0672ce
Faith Wavinya <faithwavinya09@gmail.com>
2026-05-28 12:50:27 +0300

    Add asset and asset allocation models

---
+++ app/models/asset_allocation.py
@@ -0,0 +1,26 @@
+from sqlmodel import SQLModel, Field,Relationship
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql
+import UUID
+import uuid
+import enum
+from datetime import date
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .assets import Asset
+    from .users import User
+
+
+class AssetAllocation(SQLModel, table=True):
+    __tablename__ = "asset_allocations"
+    allocation_id: Optional[int] = Field(primary_key=True)
+    asset_id: int = Field(foreign_key="assets.assets_id")
+    staff_id: int = uuid.UUID = Field(sa_column= Column(UUID(as_uuid=True), nullable=False, unique=True))
+    allocation_date: date
+    return_date: Optional[date] = Field(default=None)
+    notes: Optional[str] = Field(default=None)
+       
+
+    asset: Optional["Asset"] = Relationship(back_populates="asset_allocations")
+    staff: Optional["User"] = Relationship(back_populates="asset_allocations")
